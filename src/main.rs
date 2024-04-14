a0f824ffda57e28a2a391d6781dcf66f786a9644
Nathan LeRoy <NLeRoy917@gmail.com>
2024-04-13 20:32:10 -0400

    change listening port for fly.io

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() {
         .route("/test", get(routes::show_form));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     tracing::info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
