c43e342131aa050cbcacb0e45c4b9426db4ac06c
Nathan LeRoy <NLeRoy917@gmail.com>
2024-05-16 16:59:13 -0400

    update name... for now

--- textractor-py/src/lib.rs
+++ textractor-py/src/lib.rs
@@ -4,7 +4,7 @@ pub mod extraction;
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn textractor(_py: Python, m: &PyModule) -> PyResult<()> {
+fn textractors(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(extraction::py_extract_text_from_file, m)?)?;
     m.add_function(wrap_pyfunction!(extraction::py_extract_text_from_bytes, m)?)?;
     Ok(())
